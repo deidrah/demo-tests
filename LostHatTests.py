ade6af792b3d1cb422137d4409135c89e7428327
deidrah <kumkwaczka@gmail.com>
2020-02-01 13:12:40 +0100

    Refactoring the code

--- LostHatTests.py
+++ LostHatTests.py
@@ -16,57 +16,50 @@ class LostHatTests(unittest.TestCase):
     def tearDown(self):
         self.driver.quit()
 
+    def user_login(self, driver, user_email, user_pass):
+        # finding login input box and sending value
+        login_input_element = driver.find_element_by_xpath('//*[@type="email"]')
+        login_input_element.send_keys(user_email)
+        # finding password input box and sending value
+        login_input_element = driver.find_element_by_xpath('//*[@type="password"]')
+        login_input_element.send_keys(user_pass)
+        # finding button 'SIGN IN'
+        button_next_element = driver.find_element_by_xpath('//*[@id="submit-login"]')
+        button_next_element.click()
+
+    def assert_element_text(self, driver, xpath, expected_text):
+        element = driver.find_element_by_xpath(xpath)
+        element_text = element.text
+        self.assertEqual(expected_text, element_text, f'Expected text differ from actual on page: {driver.current_url}')
+
     def test_login_text_header(self):
         expected_text = 'Log in to your account'
+        xpath = '//header[@class="page-header"]'
         driver = self.driver
         driver.get(self.login_url)
-        header_element = driver.find_element_by_xpath('//header[@class="page-header"]')
-        header_element_text = header_element.text
-        self.assertEqual(expected_text, header_element_text,
-                         f'Expected text differ from actual for page url: {self.login_url}')
+        self.assert_element_text(driver, xpath, expected_text)
 
     def test_correct_login(self):
         # expected_text is a user name and user surname used during registration
         expected_text = 'Adam Testowy'
+        user_name_xpath = '//a[@class="account"]/*[@class="hidden-sm-down"]'
         user_email = 'adam@testowy.pl'
         user_pass = 'test123'
         driver = self.driver
         driver.get(self.login_url)
-        # finding login input box and sending value
-        login_input_element = driver.find_element_by_xpath('//*[@type="email"]')
-        login_input_element.send_keys(user_email)
-        # finding password input box and sending value
-        login_input_element = driver.find_element_by_xpath('//*[@type="password"]')
-        login_input_element.send_keys(user_pass)
-        # finding button 'sign in'
-        button_next_element = driver.find_element_by_xpath('//*[@id="submit-login"]')
-        button_next_element.click()
-        header_element = driver.find_element_by_xpath('//a[@class="account"]/*[@class="hidden-sm-down"]')
-        header_element_text = header_element.text
-        self.assertEqual(expected_text, header_element_text,
-                         f'Expected title differ from actual title for page url: {self.login_url}')
+        self.user_login(driver, user_email, user_pass)
+        self.assert_element_text(driver, user_name_xpath, expected_text)
 
     def test_incorrect_login(self):
         # expected_text is a warning message about authentication failed
         expected_text = 'Authentication failed.'
+        alert_xpath = '//*[@class="alert alert-danger"]'
         user_email = 'invalid@test.test'
         user_pass = 'abc123'
         driver = self.driver
         driver.get(self.login_url)
-        # finding login input box and sending value
-        login_input_element = driver.find_element_by_xpath('//*[@type="email"]')
-        login_input_element.send_keys(user_email)
-        # finding password input box and sending value
-        login_input_element = driver.find_element_by_xpath('//*[@type="password"]')
-        login_input_element.send_keys(user_pass)
-        # finding button 'SIGN IN' and clicking it
-        button_next_element = driver.find_element_by_xpath('//*[@id="submit-login"]')
-        button_next_element.click()
-
-        alert_element = driver.find_element_by_xpath('//*[@class="alert alert-danger"]')
-        alert_element_text = alert_element.text
-        self.assertEqual(expected_text, alert_element_text,
-                         f'Expected text differ from actual for page url: {self.login_url}')
+        self.user_login(driver, user_email, user_pass)
+        self.assert_element_text(driver, alert_xpath, expected_text)
 
     def test_check_product_name(self):
         expected_product_name = 'HUMMINGBIRD PRINTED T-SHIRT'
