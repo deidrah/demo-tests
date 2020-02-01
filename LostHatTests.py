79df624554d47b9f21ccb82e05632ca4ff9be003
deidrah <kumkwaczka@gmail.com>
2020-02-01 15:31:09 +0100

    adding new tests and refactoring

--- LostHatTests.py
+++ LostHatTests.py
@@ -4,7 +4,6 @@ from helpers import functional_helpers as fh
 
 
 class LostHatTests(unittest.TestCase):
-
     @classmethod
     def setUp(self):
         self.base_url = 'https://autodemo.testoneo.com/en/'
@@ -17,24 +16,12 @@ class LostHatTests(unittest.TestCase):
     def tearDown(self):
         self.driver.quit()
 
-    def assert_element_text(self, driver, xpath, expected_text):
-        """Comparing expected text with observed value from web element
-
-           :param driver: webdriver instance
-           :param xpath: xpath to element with text to be observed
-           :param expected_text: text what we expecting to be found
-           :return: None
-        """
-        element = driver.find_element_by_xpath(xpath)
-        element_text = element.text
-        self.assertEqual(expected_text, element_text, f'Expected text differ from actual on page: {driver.current_url}')
-
     def test_login_text_header(self):
         expected_text = 'Log in to your account'
-        xpath = '//header[@class="page-header"]'
+        header_xpath = '//header[@class="page-header"]'
         driver = self.driver
         driver.get(self.login_url)
-        self.assert_element_text(driver, xpath, expected_text)
+        self.assert_element_text(driver, header_xpath, expected_text)
 
     def test_correct_login(self):
         # expected_text is a user name and user surname used during registration
@@ -60,18 +47,95 @@ class LostHatTests(unittest.TestCase):
 
     def test_check_product_name(self):
         expected_product_name = 'HUMMINGBIRD PRINTED T-SHIRT'
+        name_xpath = '//*[@class="col-md-6"]//*[@itemprop="name"]'
         driver = self.driver
         driver.get(self.sample_product_url)
-        name_element = driver.find_element_by_xpath('//*[@class="col-md-6"]//*[@itemprop="name"]')
-        name_element_text = name_element.text
-        self.assertEqual(expected_product_name, name_element_text,
-                         f'Expected text differ from actual for page url: {self.sample_product_url}')
+        self.assert_element_text(driver, name_xpath, expected_product_name)
 
     def test_check_product_price(self):
         expected_product_price = 'PLN23.52'
+        price_xpath = '//*[@class="current-price"]//*[@itemprop="price"]'
         driver = self.driver
         driver.get(self.sample_product_url)
-        price_element = driver.find_element_by_xpath('//*[@class="current-price"]//*[@itemprop="price"]')
-        price_element_text = price_element.text
-        self.assertEqual(expected_product_price, price_element_text,
-                         f'Expected text differ from actual for page url: {self.sample_product_url}')
+        self.assert_element_text(driver, price_xpath, expected_product_price)
+
+    def assert_element_text(self, driver, xpath, expected_text):
+        """Comparing expected text with observed value from web element
+
+         :param driver: webdriver instance
+         :param xpath: xpath to element with text to be observed
+         :param expected_text: text what we expecting to be found
+         :return: None
+        """
+        element = driver.find_element_by_xpath(xpath)
+        element_text = element.text
+        self.assertEqual(expected_text, element_text, f'Expected text differ from actual on page: {driver.current_url}')
+
+
+class LostHatFrontPageTests(unittest.TestCase):
+    @classmethod
+    def setUp(self):
+        self.base_url = 'https://autodemo.testoneo.com/en/'
+        self.driver = webdriver.Chrome(executable_path=r"C:\Users\Dominika\Downloads\chromedriver_win32("
+                                                       r"1)\chromedriver.exe")
+
+    @classmethod
+    def tearDown(self):
+        self.driver.quit()
+
+    def test_slider_presention(self):
+        slider_xpath = '//*[@id="carousel"]'
+        driver = self.driver
+        driver.get(self.base_url)
+        driver.find_element_by_xpath(slider_xpath)
+
+    def test_slider_minimum_size(self):
+        expected_min_height = 300
+        expected_min_width = 600
+        slider_xpath = '//*[@id="carousel"]'
+        driver = self.driver
+        driver.get(self.base_url)
+        slider_element = driver.find_element_by_xpath(slider_xpath)
+        actual_slider_height = slider_element.size['height']
+        actual_slider_width = slider_element.size['width']
+        with self.subTest('Element height'):
+            self.assertLess(expected_min_height, actual_slider_height,
+                            f'Element height found by xpath {slider_xpath} on page {driver.current_url} is smaller '
+                            f'than expected {expected_min_height}px')
+        with self.subTest('Element width'):
+            self.assertLess(expected_min_width, actual_slider_width,
+                            f'Element width found by xpath {slider_xpath} on page {driver.current_url} is smaller '
+                            f'than expected {expected_min_width}px')
+
+    def test_slider_contain_exact_number_of_slides(self):
+        expected_number_of_slides = 3
+        slides_xpath = '//*[@id="carousel"]/ul/li'
+        driver = self.driver
+        driver.get(self.base_url)
+        slider_elements = driver.find_elements_by_xpath(slides_xpath)
+        actual_number_of_slides = len(slider_elements)
+        self.assertEqual(expected_number_of_slides, actual_number_of_slides,
+                         f'Slides number differ for page {self.base_url}')
+
+    def test_slides_required_title_text(self):
+        expected_text_included_in_slide = 'sample'
+        slides_titles_xpath = '//*[@id="carousel"]/ul/li//*[contains(@class, "text-uppercase")]'
+        driver = self.driver
+        driver.get(self.base_url)
+        title_elements = driver.find_elements_by_xpath(slides_titles_xpath)
+        for title_element in title_elements:
+            title_element_text = title_element.get_attribute("textContent")
+            title_element_text_lower = title_element_text.lower()
+            with self.subTest(title_element_text_lower):
+                self.assertIn(expected_text_included_in_slide, title_element_text_lower,
+                              f"Slides does not contain expected text for page {self.base_url}")
+
+    def test_number_of_featured_products(self):
+        expected_number_of_products = 8
+        product_xpath = '//*[@class="product-miniature js-product-miniature"]'
+        driver = self.driver
+        driver.get(self.base_url)
+        product_elements = driver.find_elements_by_xpath(product_xpath)
+        actual_number_of_products = len(product_elements)
+        self.assertEqual(expected_number_of_products, actual_number_of_products,
+                         f'Products number differ for page {self.base_url}')
